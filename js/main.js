f996ba43be42e14bd6e3d783d354c3602c21f576
kadz25 <ovcharenko.07m@gmail.com>
2025-03-26 23:00:20 +0100

    blog page added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active"); // Добавляем класс "active" для анимации иконки
   nav.classList.toggle("open"); // Показываем или скрываем меню
 });
+
+document.querySelectorAll(".toggle-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const content = this.parentElement.nextElementSibling;
+
+    // Если блок уже открыт
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      this.innerHTML = "&#x25BC;"; // Стрелочка вниз
+    } else {
+      // Задаем высоту блока равной его реальной высоте
+      content.style.maxHeight = content.scrollHeight + "px";
+      this.innerHTML = "&#x25B2;"; // Стрелочка вверх
+    }
+  });
+});
