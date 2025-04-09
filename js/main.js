00e611251b48b414072d31533158da7035532976
kadz25 <ovcharenko.07m@gmail.com>
2025-04-09 20:29:29 +0200

    js img change

--- js/main.js
+++ js/main.js
@@ -22,3 +22,39 @@ document.querySelectorAll(".toggle-btn").forEach((button) => {
     }
   });
 });
+
+// смена фото главной секции
+const images = [
+  "url(images/gray-building.jpg)",
+  "url(images/smog.jpg)",
+  "url(images/metro-shadow.jpg)",
+  "url(images/szpiegowo-building.jpg)",
+];
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+slides[0].style.backgroundImage = images[3];
+slides[0].classList.add("active");
+
+function goToSlide(index) {
+  slides[current].classList.remove("active");
+  current = index;
+  slides[current].style.backgroundImage = images[current];
+  slides[current].classList.add("active");
+}
+
+function nextSlide() {
+  const next = (current + 1) % slides.length;
+  goToSlide(next);
+}
+
+function prevSlide() {
+  const prev = (current - 1 + slides.length) % slides.length;
+  goToSlide(prev);
+}
+
+document.getElementById("next").addEventListener("click", nextSlide);
+document.getElementById("prev").addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 5000);
